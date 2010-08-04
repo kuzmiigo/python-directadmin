c5c97fc11e7cc659ed603fbba88571d95790773a
andresgattinoni <andresgattinoni@ead67abe-f584-11de-8f43-4547254af6c6>
2010-08-04 03:50:44 +0000

    - Massive directory tree and files restructuration - Creation of setup script for distribution
    git-svn-id: http://python-directadmin.googlecode.com/svn/trunk@49 ead67abe-f584-11de-8f43-4547254af6c6

---
+++ setup.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from distutils.core import setup
+
+setup(name='python-directadmin', \
+      version='0.3', \
+      description='Python implementation of Directadmin\'s Web API', \
+      author='Andrés Gattinoni', \
+      author_email='andresgattinoni@gmail.com', \
+      url='http://code.google.com/p/python-directadmin/', \
+      packages=['directadmin'], \
+      scripts=['scripts/da_suspension', 'scripts/da_console'], \
+      license='GPL v.3', \
+      classifiers=[
+        'Development Status :: 3 - Alpha', \
+        'Environment :: Console', \
+        'Environment :: Web Environment', \
+        'Intended Audience :: Developers', \
+        'License :: OSI Approved :: GNU General Public License (GPL)', \
+        'Operating System :: POSIX', \
+        'Programming Language :: Python', \
+        'Topic :: Internet', \
+        'Topic :: Software Development :: Libraries :: Python Modules', \
+        'Topic :: System :: Monitoring' \
+      ])
